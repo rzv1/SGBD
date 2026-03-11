e543a58fdf3417267e14e85cb0e10e1d85651e1d
rzv1 <rzv.dev@gmail.com>
2026-03-11 13:33:24 +0200

    Add: MVVM complete

---
+++ L01/App.axaml.cs
@@ -0,0 +1,52 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Core;
+using Avalonia.Data.Core.Plugins;
+using System.Linq;
+using Avalonia.Markup.Xaml;
+using L01.Models;
+using L01.ViewModels;
+using L01.Views;
+
+namespace L01;
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
+            // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
+            DisableAvaloniaDataAnnotationValidation();
+            string path = "Host=192.168.0.45;Port=5432;Database=gym;Username=postgres;Password=rzv";
+            DatabaseManager manager = new DatabaseManager(path);
+            CustomerRepo cRepo = new CustomerRepo(manager);
+            PaymentRepo pRepo = new PaymentRepo(manager);
+            desktop.MainWindow = new MainWindow
+            {
+                DataContext = new MainWindowViewModel(cRepo, pRepo),
+            };
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+
+    private void DisableAvaloniaDataAnnotationValidation()
+    {
+        // Get an array of plugins to remove
+        var dataValidationPluginsToRemove =
+            BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
+
+        // remove each entry found
+        foreach (var plugin in dataValidationPluginsToRemove)
+        {
+            BindingPlugins.DataValidators.Remove(plugin);
+        }
+    }
+}
---
+++ L01/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.JavaScript;
+using Avalonia.Utilities;
+using L01.Models;
+
+namespace L01.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    private readonly CustomerRepo? _customerRepo;
+    private readonly PaymentRepo? _paymentRepo;
+
+    public MainWindowViewModel()
+    {
+        Customers.Add(new Customer("John", "Labu") {Id = 1});
+    }
+    public MainWindowViewModel(CustomerRepo cRepo, PaymentRepo pRepo)
+    {
+        _customerRepo = cRepo;
+        _paymentRepo = pRepo;
+        RefreshCustomers();
+    }
+    
+    private Customer? _selectedCustomer;
+    private Payment? _selectedPayment;
+    public ObservableCollection<Customer> Customers { get; } = new();
+    public ObservableCollection<Payment> Payments { get; } = new();
+
+    public Customer? SelectedCustomer
+    {
+        get => _selectedCustomer;
+        set
+        {
+            SetProperty(ref _selectedCustomer, value);
+            LoadPayments();
+        }
+    }
+
+    public Payment? SelectedPayment
+    {
+        get => _selectedPayment;
+        set => _selectedPayment = value;
+    }
+
+    private string? _errorMessage;
+
+    public string? ErrorMessage
+    {
+        get => _errorMessage;
+        set => SetProperty(ref _errorMessage, value);
+    }
+    private void ClearError() => ErrorMessage = null;
+    public string NewAmount { get; set; } = ""; 
+    public string NewType { get; set; } = "";
+    public string NewBank { get; set; } = "";
+
+    private void LoadPayments()
+    {
+        if (SelectedCustomer == null) return;
+
+        try
+        {
+            var data = _paymentRepo!.FindAll(SelectedCustomer.Id);
+            Payments.Clear();
+            foreach (var p in data)
+            {
+                Payments.Add(p);
+            }
+        }
+        catch (Exception e)
+        {
+            ErrorMessage = $"Payments load error: {e.Message}";
+        }
+    }
+
+    public void RefreshCustomers()
+    {
+        try
+        {
+            Customers.Clear();
+            var data = _customerRepo!.FindAll();
+            foreach (var c in data)
+            {
+                Customers.Add(c);
+            }
+        }
+        catch (Exception e)
+        {
+            ErrorMessage = $"Refresh error: {e.Message}";
+        }
+    }
+
+    public void AddPayment()
+    {
+        try
+        {
+            decimal parsed = 0;
+            decimal.TryParse(NewAmount, out parsed);
+            if (parsed == 0)
+                ErrorMessage = $"Invalid amount!";
+            else if (string.IsNullOrEmpty(NewType))
+                ErrorMessage = $"Invalid type (empty)!";
+            else if (SelectedCustomer == null)
+                ErrorMessage = $"Customer not selected!";
+            else
+            {
+                _paymentRepo!.Save(new Payment(parsed, NewType, NewBank, SelectedCustomer!.Id));
+                LoadPayments();
+                ErrorMessage = $"Operation successful";
+            }
+        }
+        catch (Exception e)
+        {
+            ErrorMessage = $"Add payment error: {e.Message}";
+        }
+    }
+
+    public void UpdatePayment()
+    {
+        try
+        {
+            if (SelectedPayment == null)
+            {
+                ErrorMessage = $"Select a payment first";
+                return;
+            }
+
+            _paymentRepo?.Update(SelectedPayment);
+            LoadPayments();
+            ErrorMessage = "Operation successful";
+        }
+        catch (Exception e)
+        {
+            ErrorMessage = $"Update error: {e.Message}";
+        }
+    }
+
+    public void DeletePayment()
+    {
+        try
+        {
+            if (SelectedPayment == null)
+                ErrorMessage = $"Select a payment first";
+            else
+            {
+                _paymentRepo!.Delete(SelectedPayment.Id);
+                LoadPayments();
+                ErrorMessage = "Operation successful";
+            }
+        }
+        catch (Exception e)
+        {
+            ErrorMessage = $"Delete error: {e.Message}";
+        }
+    }
+}
